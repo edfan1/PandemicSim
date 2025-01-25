637da1b39b42dbe93ca72e80f529f729ec25edab
edfan1 <91496311+edfan1@users.noreply.github.com>
2025-01-25 16:23:21 -0500

    Removed uncessary repeat code from sim (code can be found in temp_sim)

--- backend/sim.py
+++ backend/sim.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 from city import City
 
 city = City()
@@ -26,31 +25,3 @@ def construct_stores(x):
 def add_homes(x):
     for i in range(x):
         city.add_home(id=i)
-
-susceptible_counts = []
-infected_counts = []
-recovered_counts = []
-
-city.inject_patient_zero()
-
-for i in range(50):
-    for i in range(24):
-        city.update_city()
-        counts = city.get_counts()
-        susceptible_counts.append(counts['susceptible'])
-        infected_counts.append(counts['infected'])
-        recovered_counts.append(counts['recovered'])
-
-
-# for types in city.city.keys():
-#     for building in city.city[types]:
-#         print(building.__str__())
-
-plt.plot(susceptible_counts, label='Susceptible')
-plt.plot(infected_counts, label='Infected')
-plt.plot(recovered_counts, label='Recovered')
-plt.xlabel('Time (updates)')
-plt.ylabel('Number of People')
-plt.title('Pandemic Simulation in Building')
-plt.legend()
-plt.show()
