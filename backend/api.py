deb4c7e86c5207e44d8f2dfd04690918bda3339e
edfan1 <91496311+edfan1@users.noreply.github.com>
2025-01-25 18:32:05 -0500

    Added building_id to fetched feilds and streamlined data loading process

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,6 @@
 
 from fastapi import APIRouter, HTTPException
-from typing import List
+from typing import List, Dict
 from models import *
 from sim import *
 
@@ -8,15 +8,13 @@ router = APIRouter()
 
 # Endpoint to receive building data and run the simulation
 @router.post("/run-simulation", response_model=str)
-async def run_simulation(buildings: List[Building]):
-    if not buildings:
+async def run_simulation(building_types: Dict[str, List[Building]]):
+    if not building_types:
         raise HTTPException(status_code=400, detail="No building data provided")
 
-    # Example simulation: Mark some buildings as infected
-    HospitalNames = [Building.name for Building in buildings]
-    construct_hospitals(len(HospitalNames), 10, 10)
-    # replace numbers with user inputs for avgbeds and avgworkers
-
+    for building_type, buildings in building_types.items():
+        for building in buildings:
+            construct_building(building_type, building.building_id)
     # Return the simulation results
     return "hello"
 
--- backend/sim.py
+++ backend/sim.py
@@ -20,21 +20,8 @@ def construct_hospitals(num, avgBeds, avgWorkers):
     for i in range(num):
         city.construct_hospital(1, avgBeds, avgWorkers)
 
-def construct_restaurants(x):
-    for i in range(x):
-        city.construct_building('restaurant', 1)
-
-def construct_schools(x):
-    for i in range(x):
-        city.construct_building('school', 1)
-
-def construct_offices(x):
-    for i in range(x):
-        city.construct_building('office', 1)
-
-def construct_stores(x):
-    for i in range(x):
-        city.construct_building('store', 1)
+def construct_building(typestr, id):
+        city.construct_building(typestr, id)
 
 def add_homes(x):
     for i in range(x):
